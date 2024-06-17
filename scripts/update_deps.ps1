3b395f33565658bf53ad3eb29be737327f3c293f
Ed_ <edwardgz@gmail.com>
2024-06-17 16:56:51 -0400

    Ignore thirdparty and toolchain directories & updates to build scripts

--- scripts/update_deps.ps1
+++ scripts/update_deps.ps1
@@ -34,78 +34,15 @@ $result = verify-path $path_toolchain
 
 $binaries_dirty = $false
 
-function Update-GitRepo
-{
-	param( [string] $path, [string] $url, [string] $build_command )
-
-	if ( $build_command -eq $null ) {
-		write-host "Attempted to call Update-GitRepo without build_command specified"
-		return
-	}
-
-	$repo_name = $url.Split('/')[-1].Replace('.git', '')
-
-	$last_built_commit = join-path $path_build "last_built_commit_$repo_name.txt"
-	if ( -not(test-path -Path $path))
-	{
-		write-host "Cloining repo from $url to $path"
-		git clone $url $path
-
-		write-host "Building $url"
-		push-location $path
-		& "$build_command"
-		pop-location
-
-		git -C $path rev-parse HEAD | out-file $last_built_commit
-		$script:binaries_dirty = $true
-		write-host
-		return
-	}
-
-	git -C $path fetch
-	$latest_commit_hash = git -C $path rev-parse '@{u}'
-	$last_built_hash    = if (Test-Path $last_built_commit) { Get-Content $last_built_commit } else { "" }
-
-	if ( $latest_commit_hash -eq $last_built_hash ) {
-		write-host
-		return
-	}
-
-	write-host "Build out of date for: $path, updating"
-	write-host 'Pulling...'
-	git -C $path pull
-
-	write-host "Building $url"
-	push-location $path
-	& $build_command
-	pop-location
-
-	$latest_commit_hash | out-file $last_built_commit
-	$script:binaries_dirty = $true
-	write-host
-}
-
-push-location $path_thirdparty
-
-Update-GitRepo -path $path_odin     -url $url_odin_repo -build_command '.\scripts\build.ps1'
-Update-GitRepo -path $path_sokol    -url $url_sokol     -build_command '.\build_windows.ps1'
-Update-GitRepo -path $path_harfbuzz -url $url_harfbuzz  -build_command '.\scripts\build.ps1'
-
-function clone-gitrepo { param( [string] $path, [string] $url )
-	if (test-path $path) {
-		# git -C $path pull
-	}
-	else {
-		Write-Host "Cloning $url ..."
-		git clone $url $path
-	}
-}
-
 clone-gitrepo $path_backtrace   $url_backtrace_repo
 clone-gitrepo $path_freetype    $url_freetype
 clone-gitrepo $path_ini_parser  $url_ini_parser
 clone-gitrepo $path_ini_parser  $url_ini_parser
 clone-gitrepo $path_sokol_tools $url_sokol_tools
+
+Update-GitRepo -path $path_odin     -url $url_odin_repo -build_command '.\scripts\build.ps1'
+Update-GitRepo -path $path_sokol    -url $url_sokol     -build_command '.\build_windows.ps1'
+Update-GitRepo -path $path_harfbuzz -url $url_harfbuzz  -build_command '.\scripts\build.ps1'
 
 $path_vendor        = join-path $path_odin          'vendor'
 $path_vendor_raylib = join-path $path_vendor        'raylib'
@@ -113,6 +50,8 @@ $path_freetype_dlls = join-path $path_freetype      'binaries/release'
 $path_harfbuzz_dlls = join-path $path_harfbuzz      'lib/win64'
 $path_raylib_dlls   = join-path $path_vendor_raylib 'windows'
 $path_sokol_dlls    = join-path $path_thirdparty    'sokol'
+
+push-location $path_thirdparty
 
 if ( $binaries_dirty -or $true )
 {
